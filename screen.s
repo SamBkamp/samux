7b954f27ef084910f800d4dc51020ad6d1d2b179
Sam Bonnekamp <sam@bonnekamp.net>
2026-01-12 16:55:41 +0800

    changed go to line function to be quicker and more reliable

--- screen.s
+++ screen.s
@@ -1,4 +1,7 @@
+;;sreg
+;;clobbers portb and porta
 init_screen:
+        pha
         lda #%00000001          ;clear display
         sta PORTB
         jsr lcd_instruction_send
@@ -18,8 +21,42 @@ init_screen:
         lda #%00000110          ;entry mode, incr, no scroll
         sta PORTB
         jsr lcd_instruction_send
+        pla
         rts
 
+;;sreg
+;;clobbers porta
+clear_screen:
+        pha
+        lda #$1                 ;clear display command
+        sta PORTB
+        jsr lcd_instruction_send
+        pla
+        rts
+
+;;sreg
+;;clobers porta
+return_home:
+        sei
+        pha
+        lda #%00000010                ;return home
+        sta PORTB
+        jsr lcd_instruction_send
+        pla
+        cli
+        rts
+
+;;takes line in reg a (only reads lsb)
+go_to_line:
+        pha
+        and #$ff
+        beq exit_go_to_line     ;if a is 0, we're already on line 0, exit
+        lda #%11000000          ;use set ddram to move to line 2 (quicker, no need return home or right shift loop). address set to 0x40 which is start of line 2
+        sta PORTB
+        jsr lcd_instruction_send
+exit_go_to_line:
+        pla
+        rts
 
 ;; sreg
 lcd_wait:
@@ -42,19 +79,18 @@ reread:
         sta DDRB
         pla
         rts
-       
+
 ;; changes a
 lcd_instruction_send:
-        jsr lcd_wait
         lda #E                  ;toggle enable
         sta PORTA
         lda #0
         sta PORTA
+        jsr lcd_wait
         rts
 
 ;; changes port a
 print_char:
-        jsr lcd_wait
         sta PORTB
         lda #RS                  ;turn on RS
         sta PORTA
@@ -63,4 +99,5 @@ print_char:
         sta PORTA
         lda #RS                 ;un toggle enable but keep RS
         sta PORTA
+        jsr lcd_wait
         rts
--- splash_screen.s
+++ splash_screen.s
@@ -35,12 +35,24 @@ _start:
         cli
 
         ldx #$0
-print_loop:
+print_splash_loop:
         lda splash, x
+        beq prep_version_print
+        jsr print_char
+        inx
+        jmp print_splash_loop
+
+prep_version_print:
+        lda #$01
+        jsr go_to_line
+        ldx #$0
+
+print_version_loop:
+        lda version_num, x
         beq _loop
         jsr print_char
         inx
-        jmp print_loop
+        jmp print_version_loop
 
 _loop:
         jsr toggle_led

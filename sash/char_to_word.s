10f78807ff0321e1e38c10c47a6678e77d5a83cd
Sam Bonnekamp <sam@bonnekamp.net>
2026-02-09 00:28:38 +0800

    read command can take ascii hex letters now too
    caps independent

--- sash/char_to_word.s
+++ sash/char_to_word.s
@@ -1,7 +1,14 @@
 add_nibble_to_word:
         pha
         phx
+        cmp #"9"
+        bcs nibble_is_letter
         eor #$30                ;converts ascii 0-9 to digit
+        jmp nibble_word_start
+nibble_is_letter:
+        and #%11011111          ;sets bit 5 to 0 (char to lowercase)
+        sbc #("A"-10)           ;converts ascii A-F to digit
+nibble_word_start:
         ldx #$04
 ;;shift word one nibble over to make space for next nibble
 nibble_word_loop:
